90840efb93d811b4ff5b3f5db4238691f8bb562d
Guillermo Eguizabal <cgeguizabal@gmail.com>
2025-12-20 09:17:11 -0600

    Added signup Page

--- Interguia_SEO_Dashboard_Front_End/src/App.jsx
+++ Interguia_SEO_Dashboard_Front_End/src/App.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 import LoginPage from "./pages/LoginPage.jsx";
+import SignupPage from "./pages/SignupPage.jsx";
 
 function App() {
   return (
     <main>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/Signup" element={<SignupPage />} />
       </Routes>
     </main>
   );
---
+++ Interguia_SEO_Dashboard_Front_End/src/pages/SignupPage.jsx
@@ -0,0 +1,9 @@
+function SignupPage() {
+  return (
+    <div>
+      <h1>This is the SignupPage</h1>
+    </div>
+  );
+}
+
+export default SignupPage;
