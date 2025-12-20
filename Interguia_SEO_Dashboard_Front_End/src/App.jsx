53799e951222b59ddb9fb4b7043658b2aef81534
Guillermo Eguizabal <cgeguizabal@gmail.com>
2025-12-20 09:03:58 -0600

    Added React-router and LoginPage

--- Interguia_SEO_Dashboard_Front_End/src/App.jsx
+++ Interguia_SEO_Dashboard_Front_End/src/App.jsx
@@ -1,7 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
+import LoginPage from "./pages/LoginPage.jsx";
+
 function App() {
-  return <h1 className="test">INTERGUIA SEO DASHBOARD, Test of clone</h1>;
+  return (
+    <main>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
---
+++ Interguia_SEO_Dashboard_Front_End/src/pages/LoginPage.jsx
@@ -0,0 +1,9 @@
+function LoginPage() {
+    return (
+        <div>
+            <h1>This is log in LoginPage</h1>
+        </div>
+    )
+}
+
+export default LoginPage
