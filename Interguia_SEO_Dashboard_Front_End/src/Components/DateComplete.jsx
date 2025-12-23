72176781ebe17f7b690b9daf75182496cfcd998f
Guillermo Eguizabal <cgeguizabal@gmail.com>
2025-12-23 16:00:31 -0600

    Agregado fecha completa

--- Interguia_SEO_Dashboard_Front_End/src/Components/DateComplete.jsx
+++ Interguia_SEO_Dashboard_Front_End/src/Components/DateComplete.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import DateMonthYearPickerStyles from "../styles/components/DateMonthYearPicker.module.scss";
 
 export default function DateComplete() {
+  const Months = [
+        "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+        "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+    ];
+
+    const StartYear = 1980;
+    const CurrentYear = new Date().getFullYear();
   return (
-    <div>DateComplete</div>
+    <>
+      {/*  Mes Inicio  */}
+      <select className={DateMonthYearPickerStyles.select}>
+                  {Months.map((month, index) => (
+                      <option className={DateMonthYearPickerStyles.option} key={index} value={month}>{month}</option>
+                  ))}
+              </select>
+      {/* Mes Fin */}
+      <select className={DateMonthYearPickerStyles.select}>
+                  {Months.map((month, index) => (
+                      <option className={DateMonthYearPickerStyles.option} key={index} value={month}>{month}</option>
+                  ))}
+              </select>
+      {/* Año  */}
+              <select className={DateMonthYearPickerStyles.select}>
+                  {Array.from({ length: CurrentYear - StartYear + 1}, (_, i) => CurrentYear - i).map(year => 
+                  (<option className={DateMonthYearPickerStyles.option}
+                  key={year} value={year}>{year}</option>))}
+              </select>
+    
+    </>
   )
 }
--- Interguia_SEO_Dashboard_Front_End/src/Components/FinancialRatios.jsx
+++ Interguia_SEO_Dashboard_Front_End/src/Components/FinancialRatios.jsx
@@ -7,6 +7,7 @@ import Availability from './FinacialRatiosComponents/Availability';
 import Rentability from './FinacialRatiosComponents/Rentability';
 import Efficiency from './FinacialRatiosComponents/Efficiency';
 import Indebtedness from './FinacialRatiosComponents/Indebtedness';
+import DateComplete from './DateComplete';
 
 export default function FinancialRatios() {
 
@@ -25,7 +26,7 @@ export default function FinancialRatios() {
       className={`${FinancialRatiosStyle.menu_button}
        ${selectedMenu === index ? FinancialRatiosStyle.menu_button_active : ''}`} key={index}>{item}</button>)}
     </div>
-    <div className={FinancialRatiosStyle.date_container}>Date</div>
+    <div className={FinancialRatiosStyle.date_container}><DateComplete/></div>
     <div className={FinancialRatiosStyle.content_container}>
       {selectedMenu === 0 && <Liquidity/>}
       {selectedMenu === 1 && <Availability/>}
