ebc514c6f9b04374e264cad751a3ced7cfb9863d
Guillermo Eguizabal <cgeguizabal@gmail.com>
2025-12-26 16:25:31 -0600

    agregado grafica componentes

---
+++ Interguia_SEO_Dashboard_Front_End/src/Components/Charts/Card_Small_TextLeft.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Card_Small_TextLeft() {
+  return (
+    <div>Card_Small_TextLeft</div>
+  )
+}
---
+++ Interguia_SEO_Dashboard_Front_End/src/Components/Charts/Card_Small_TextRight.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Card_Small_TextRight() {
+  return (
+    <div>Card_Small_TextRight</div>
+  )
+}
